1da89b98959b3e0a904202b22b009663bad1129d
Ian Cruz Oniszczuk <ioniszczuk@itba.edu.ar>
2025-06-18 17:33:09 -0300

    [FEAT] Creacion de tablas + insercion de datos

--- init.sql
+++ init.sql
@@ -4,33 +4,32 @@ CREATE TABLE proveedor (
     razon_social VARCHAR(100),
     tipo_sociedad VARCHAR(10) CHECK (tipo_sociedad IN ('SA', 'SRL', 'SAS', 'Colectiva')),
     direccion VARCHAR(255),
-    activo BOOLEAN NOT NULL,
-    habilitado BOOLEAN NOT NULL
+    activo BOOLEAN,
+    habilitado BOOLEAN
 );
 
 CREATE TABLE producto (
     id INT PRIMARY KEY,
-    descripcion VARCHAR(100) NOT NULL,
-    marca VARCHAR(50) NOT NULL,
-    categoria VARCHAR(50) NOT NULL,
-    precio DECIMAL(10, 2) NOT NULL,
-    stock INT NOT NULL,
+    descripcion VARCHAR(100),
+    marca VARCHAR(50),
+    categoria VARCHAR(50),
+    precio DECIMAL(10, 2),
+    stock INT
 );
 
 CREATE TABLE orden_pedido (
     id INT PRIMARY KEY,
-    id_proveedor INT NOT NULL,
-    fecha DATE NOT NULL,
-    FOREIGN KEY (proveedor_id) REFERENCES proveedor(id)
+    id_proveedor INT,
+    fecha DATE,
+    FOREIGN KEY (id_proveedor) REFERENCES proveedor(id)
 );
 
 CREATE TABLE detalle_orden_pedido (
-    id_pedido INT NOT NULL,
-    nro_item INT NOT NULL,
-    id_product INT NOT NULL,
-    cantidad INT NOT NULL,
+    id_pedido INT,
+    nro_item INT,
+    id_product INT,
+    cantidad INT,
     PRIMARY KEY (id_pedido, nro_item),
     FOREIGN KEY (id_pedido) REFERENCES orden_pedido(id),
     FOREIGN KEY (id_product) REFERENCES producto(id)
 );
-
---
+++ insert_data.sql
@@ -0,0 +1,17 @@
+SET datestyle TO 'DMY';
+
+COPY proveedor (id, cuit, razon_social, tipo_sociedad, direccion, activo, habilitado)
+FROM 'C:/Ian/ITBA/3er anio/bd-consigna-tp/proveedor.csv'
+DELIMITER ';' CSV HEADER;
+
+COPY producto (id, descripcion, marca, categoria, precio, stock)
+FROM 'C:/Ian/ITBA/3er anio/bd-consigna-tp/producto.csv'
+DELIMITER ';' CSV HEADER;
+
+COPY orden_pedido (id, id_proveedor, fecha)
+FROM 'C:/Ian/ITBA/3er anio/bd-consigna-tp/orden_pedido.csv'
+DELIMITER ';' CSV HEADER;
+
+COPY detalle_orden_pedido (id_pedido, nro_item, id_product, cantidad)
+FROM 'C:/Ian/ITBA/3er anio/bd-consigna-tp/detalle_orden_pedido.csv'
+DELIMITER ';' CSV HEADER;
